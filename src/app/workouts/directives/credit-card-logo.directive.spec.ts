7646601106743e03d2b48a0857c42f76b357881b
wonderlaw1 <wonderlaw12@gmail.com>
2020-03-06 22:38:24 +0200

    lesson 6 preparation

--- src/app/workouts/directives/credit-card-logo.directive.spec.ts
+++ src/app/workouts/directives/credit-card-logo.directive.spec.ts
@@ -1,8 +1,34 @@
 import { CreditCardLogoDirective } from './credit-card-logo.directive';
+import {Component, DebugElement, ViewChild} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
-describe('CreditCardLogoDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CreditCardLogoDirective();
-    expect(directive).toBeTruthy();
+
+@Component({
+  template: `<img appCreditCardLogo>`
+})
+class ImgCardLogoComponent {
+  @ViewChild(CreditCardLogoDirective) directive: CreditCardLogoDirective;
+}
+
+fdescribe('CreditCardLogoDirective', () => {
+  let component: ImgCardLogoComponent;
+  let fixture: ComponentFixture<ImgCardLogoComponent>;
+  let imgEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ImgCardLogoComponent, CreditCardLogoDirective]
+    });
+    fixture = TestBed.createComponent(ImgCardLogoComponent);
+    component = fixture.componentInstance;
+    imgEl = fixture.debugElement.query(By.directive(CreditCardLogoDirective));
+    fixture.detectChanges();
+  });
+
+  it('should be visa img displayed', () => {
+    component.directive.cardNumber = '4';
+    fixture.detectChanges();
+    expect(component.directive.imageSrc).toBe('assets/card-types/visa.png');
   });
 });
--- src/app/workouts/services/workouts.service.spec.ts
+++ src/app/workouts/services/workouts.service.spec.ts
@@ -5,25 +5,31 @@ import {WorkoutsApiService} from '../../core/services/workouts.api-service';
 import {LoaderService} from '../../core/services/loader.service';
 import {RouterTestingModule} from '@angular/router/testing';
 import {Router, RouterModule} from '@angular/router';
+import {of} from 'rxjs';
+import {Workout} from '../../core/models/workout.model';
+import SpyObj = jasmine.SpyObj;
 
 
 fdescribe('WorkoutsService', () => {
 
   let service: WorkoutsService;
-
-  const workoutsApiServiceSpy = jasmine.createSpyObj<WorkoutsApiService>('WorkoutsApiService', [
-    'getWorkout',
-    'getWorkouts',
-    'deleteWorkout',
-    'createWorkout',
-    'updateWorkout',
-  ]);
-  const loaderServiceSpy = jasmine.createSpyObj<LoaderService>('LoaderService', [
-    'hide',
-    'show'
-  ]);
+  let loaderService: SpyObj<LoaderService>;
+  let workoutsApiService: SpyObj<WorkoutsApiService>;
 
   beforeEach(() => {
+    const workoutsApiServiceSpy = jasmine.createSpyObj<WorkoutsApiService>('WorkoutsApiService', [
+      'getWorkout',
+      'getWorkouts',
+      'deleteWorkout',
+      'createWorkout',
+      'updateWorkout',
+    ]);
+
+    const loaderServiceSpy = jasmine.createSpyObj<LoaderService>('LoaderService', [
+      'hide',
+      'show'
+    ]);
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       providers: [WorkoutsService,
@@ -39,9 +45,34 @@ fdescribe('WorkoutsService', () => {
     });
 
     service = TestBed.inject(WorkoutsService);
+    loaderService = TestBed.inject(LoaderService) as SpyObj<LoaderService>;
+    workoutsApiService = TestBed.inject(WorkoutsApiService) as SpyObj<WorkoutsApiService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should delete workout by id', () => {
+    const getWorkoutsSpy = spyOn(service, 'getWorkouts' as any).and.returnValue(of({}));
+    const workoutsReceiveSpy = spyOn(service, 'onWorkoutsReceive' as any);
+    workoutsApiService.deleteWorkout.and.returnValue(of({}));
+
+    service.deleteWorkoutById(1);
+
+    expect(loaderService.show).toHaveBeenCalledTimes(1);
+    expect(workoutsApiService.deleteWorkout).toHaveBeenCalledTimes(1);
+    expect(getWorkoutsSpy).toHaveBeenCalledTimes(1);
+    expect(workoutsReceiveSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should get workout by id', () => {
+    workoutsApiService.getWorkout.and.returnValue(of({} as Workout));
+
+    service.getWorkoutById(1).subscribe();
+
+    expect(loaderService.show).toHaveBeenCalledTimes(1);
+    expect(workoutsApiService.getWorkout).toHaveBeenCalledTimes(1);
+    expect(loaderService.hide).toHaveBeenCalledTimes(1);
   });
 });
